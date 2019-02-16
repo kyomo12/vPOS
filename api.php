7feed2c55f77be200fe95c69eafa26cbb7bdb73b
Moses Chagama <mchagama@it.co.tz>
2019-02-16 20:14:35 +0300

    Cleanup

--- api.php
+++ api.php
@@ -25,41 +25,10 @@ switch($request){
 		$posRestHandler->getRequestMethod($data);
 		break;
 
-	// case "login":
-	// 	$userRestHandler = new UserRestHandler();
-	// 	$userRestHandler->login($data);
-	// 	break;
-	//
-	// case "logout":
-	// 	$userRestHandler = new UserRestHandler();
-	// 	$userRestHandler->logout($data);
-	// 	break;
-
 	case "" :
 		//404 - not found;
 		header("HTTP/1.1 404 Not Found");
 		break;
 }
 
-
-// $view = "";
-// if(isset($_GET["view"]))
-// 	$view = $_GET["view"];
-// /*
-// controls the RESTful services
-// URL mapping
-// */
-// switch($view){
-//
-// 	case "all":
-// 		// to handle REST Url /mobile/list/
-// 		echo "testsii";
-// 		$posRestHandler = new PosRestHandler();
-// 		$posRestHandler->getAllPos();
-// 		break;
-//
-// 	case "" :
-// 		//404 - not found;
-// 		break;
-// }
 ?>
